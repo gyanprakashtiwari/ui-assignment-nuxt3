d6937892cb3a7774a320fced73bb8fbe8b006364
Gyan Prakash Tiwari <gyanprakashtiwari@MacBookAir.lan>
2024-12-26 10:54:42 +0530

    update

--- components/ToolsSection.vue
+++ components/ToolsSection.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="mt-4">
-    <div class="d-flex justify-content-center align-items-center">
-      <h1 class="fw-bolder">All the tools that you need</h1>
-    </div>
-    <div class="d-flex justify-content-center align-items-center">
-      <div class="col-lg-8">
-        <p class="text-center">
-          Sit elit feugiat turpis sed integer integer accumsan turpis. Sed
-          suspendisse nec lorem mauris. Pharetra, eu imperdiet ipsum ultrices
-          amet, dui sit suspendisse.
-        </p>
+  <div class="custom-background-image">
+    <div class="mt-5 custom-pd">
+      <div class="d-flex justify-content-center align-items-center">
+        <h1 class="fw-bolder">All the tools that you need</h1>
+      </div>
+      <div class="d-flex justify-content-center align-items-center">
+        <div class="col-lg-8">
+          <p class="text-center">
+            Sit elit feugiat turpis sed integer integer accumsan turpis. Sed
+            suspendisse nec lorem mauris. Pharetra, eu imperdiet ipsum ultrices
+            amet, dui sit suspendisse.
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.custom-background-image {
+  background-image: url(/assets/icons/tools-sec.png);
+  background-size: contain; /* Makes the image cover the entire div */
+  background-position: center; /* Centers the image */
+  background-repeat: no-repeat;
+  height: 1195px;
+  width: 100%;
+}
+.custom-pd {
+  padding-top: 14rem;
+}
+</style>
