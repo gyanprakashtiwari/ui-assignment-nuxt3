6aa3bb952ee5cee89b2ecec4642d7672e5ae4ee5
Gyan Prakash Tiwari <gyanprakashtiwari@MacBookAir.lan>
2024-12-27 11:29:17 +0530

    3rd section done

--- components/AllInOneSection.vue
+++ components/AllInOneSection.vue
@@ -24,27 +24,93 @@
       </div>
       <div class="right-section col-lg-7">
         <div
-          class="bg custom-background-img outer-div d-flex flex-column justify-content-end align-items-center"
+          class="bg custom-background-img outer-div d-flex flex-column justify-content-center align-items-center"
         >
-          <div class="row">
-            <div class="d-flex space-around">
-              <div class="col-4">
-                <div>one</div>
-                <div>two</div>
-                <div>three</div>
-                <div>four</div>
+          <div class="row mt-auto">
+            <div class="d-flex space-around gap-1">
+              <div class="col-4 custom-small-card">
+                <div>
+                  <div class="bg text-wrap custom-content-feature">
+                    <p>Featured</p>
+                  </div>
+                </div>
+                <div>
+                  <div class="bg text-wrap custom-sub-heading">
+                    <p>The map of mathematics</p>
+                  </div>
+                </div>
+                <div>
+                  <div class="bg text-wrap custom-content-sub-heading">
+                    <p>
+                      Egestas elit dui scelerisque ut eu purus aliquam vitae
+                      habitasse.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    type="button"
+                    class="btn btn-outline-primary custom-btn"
+                  >
+                    Take Lesson
+                  </button>
+                </div>
               </div>
-              <div class="col-4">
-                <div>one</div>
-                <div>two</div>
-                <div>three</div>
-                <div>four</div>
+              <div class="col-4 custom-small-card">
+                <div>
+                  <div class="bg text-wrap custom-content-popular">
+                    <p>Popular</p>
+                  </div>
+                </div>
+                <div>
+                  <div class="bg text-wrap custom-sub-heading">
+                    <p>Design for how people think</p>
+                  </div>
+                </div>
+                <div>
+                  <div class="bg text-wrap custom-content-sub-heading">
+                    <p>
+                      Egestas elit dui scelerisque ut eu purus aliquam vitae
+                      habitasse.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    type="button"
+                    class="btn btn-outline-primary custom-btn"
+                  >
+                    Take Lesson
+                  </button>
+                </div>
               </div>
-              <div class="col-4">
-                <div>one</div>
-                <div>two</div>
-                <div>three</div>
-                <div>four</div>
+              <div class="col-4 custom-small-card">
+                <div>
+                  <div class="bg text-wrap custom-content-new">
+                    <p>New</p>
+                  </div>
+                </div>
+                <div>
+                  <div class="bg text-wrap custom-sub-heading">
+                    <p>International & commercial law</p>
+                  </div>
+                </div>
+                <div>
+                  <div class="bg text-wrap custom-content-sub-heading">
+                    <p>
+                      Egestas elit dui scelerisque ut eu purus aliquam vitae
+                      habitasse.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    type="button"
+                    class="btn btn-outline-primary custom-btn"
+                  >
+                    Take Lesson
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -55,6 +121,70 @@
 </template>
 
 <style scoped>
+.custom-btn {
+  /* color: #2563eb; */
+  height: 40px;
+  width: 150px;
+  font-size: 14px;
+  font-weight: 400;
+}
+/* .custom-btn :hover {
+  color: white;
+} */
+.custom-content-sub-heading {
+  font-size: 14px;
+  font-weight: 300;
+  color: #475569;
+}
+.custom-sub-heading {
+  font-size: 20px;
+  font-weight: 500;
+  color: #0f172a;
+}
+.custom-small-card {
+  display: flex;
+  flex-direction: column;
+  width: 170px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  padding: 5px;
+}
+.custom-content-new {
+  color: #166534;
+  text-align: center;
+  width: 71px;
+  height: 24px;
+  background-color: #dcfce7;
+  color: #1e40af;
+  border-radius: 4px;
+  padding: 2px;
+  font-size: 14px;
+  font-weight: 400;
+}
+.custom-content-popular {
+  width: 71px;
+  height: 24px;
+  text-align: center;
+
+  background-color: #dbeafe;
+  color: #1e40af;
+  border-radius: 4px;
+  padding: 2px;
+  font-size: 14px;
+  font-weight: 400;
+}
+.custom-content-feature {
+  width: 71px;
+  height: 24px;
+  background-color: #f3e8ff;
+  color: #6b21a8;
+  border-radius: 4px;
+  text-align: center;
+
+  padding: 2px;
+  font-size: 14px;
+  font-weight: 400;
+}
 .custom-background-img {
   background-image: url(assets/icons/sec-3-right.png);
   background-size: contain;
@@ -80,10 +210,10 @@
 }
 
 .left-section {
-  border: 2px solid red;
+  border: none;
 }
 
 .right-section {
-  border: 2px solid green;
+  border: none;
 }
 </style>
