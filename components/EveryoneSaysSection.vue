9e0a8a0f649dd25121c2f7afbdf7aec29754a7c8
Gyan Prakash Tiwari <gyanprakashtiwari@MacBookAir.lan>
2024-12-29 23:17:28 +0530

    update

--- components/EveryoneSaysSection.vue
+++ components/EveryoneSaysSection.vue
@@ -1,107 +1,219 @@
 <template>
   <div class="my-5">
-    <div class="card-header">
-      <div class="row">
-        <div class="col-6 d-flex justify-content-start align-items-center">
-          <h1 class="fw-bolder">What everyone says</h1>
-        </div>
-        <div class="col-6">
-          <div
-            class="row d-flex justify-content-end align-items-center space-between"
+    <div class="row">
+      <div class="col-6 d-flex justify-content-start align-items-center">
+        <h1 class="fw-bolder">What everyone says</h1>
+      </div>
+      <div
+        class="col-6 d-flex justify-content-end align-items-center space-between gap-1"
+      >
+        <!-- Custom Buttons -->
+        <!-- <button
+            class="carousel-control-prev arrow-btn"
+            type="button"
+            data-bs-target="#customCarousel"
+            data-bs-slide="prev"
           >
-            <div class="col-lg-2 col-md-3 col-6">
-              <button class="btn btn-outline-secondary arrow-btn">
-                <img src="/assets/icons/btn-left-arrow.png" alt="" />
-              </button>
-            </div>
-            <div class="col-lg-2 col-md-3 col-6">
-              <button class="btn btn-outline-secondary arrow-btn">
-                <img src="/assets/icons/btn-right-arrow.png" alt="" />
-              </button>
-            </div>
-          </div>
-        </div>
+            <img src="/assets/icons/btn-left-arrow.png" alt="Previous" />
+          </button>
+          <button
+            class="carousel-control-next arrow-btn"
+            type="button"
+            data-bs-target="#customCarousel"
+            data-bs-slide="next"
+          >
+            <img src="/assets/icons/btn-right-arrow.png" alt="Next" />
+          </button> -->
+
+        <button id="prevSlide" class="arrow-btn">
+          <img src="/assets/icons/btn-left-arrow.png" alt="Previous" />
+        </button>
+        <button id="nextSlide" class="arrow-btn">
+          <img src="/assets/icons/btn-right-arrow.png" alt="Next" />
+        </button>
       </div>
     </div>
-    <div
-      class="cards d-flex justofy-content-start align-items-center gap-3 py-3"
-    >
-      <div class="card" style="width: 20rem">
-        <div class="card-body">
-          <div class="card-content">
-            <p class="card-text">
-              Lacus vestibulum ultricies mi risus, duis non, volutpat nullam
-              non. Magna congue nisi maecenas elit aliquet eu sed consectetur.
-              Vitae quis cras vitae praesent morbi adipiscing purus consectetur
-              mi.
-            </p>
-          </div>
-          <div class="row mt-2">
-            <div
-              class="col-3 d-flex justify-content-center align-items-center user-thumb"
-            >
-              <img src="/assets/icons/user-thumb-1.png" alt="" />
+
+    <div id="customCarousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div
+            class="cards d-flex justofy-content-start align-items-center gap-3 py-3"
+          >
+            <div class="card" style="width: 20rem">
+              <div class="card-body">
+                <div class="card-content">
+                  <p class="card-text">
+                    Lacus vestibulum ultricies mi risus, duis non, volutpat
+                    nullam non. Magna congue nisi maecenas elit aliquet eu sed
+                    consectetur. Vitae quis cras vitae praesent morbi adipiscing
+                    purus consectetur mi.
+                  </p>
+                </div>
+                <div class="row mt-2">
+                  <div
+                    class="col-3 d-flex justify-content-center align-items-center user-thumb"
+                  >
+                    <img src="/assets/icons/user-thumb-1.png" alt="" />
+                  </div>
+                  <div
+                    class="col-9 d-flex flex-column justify-content-center align-items-start"
+                  >
+                    <div class="card-author">Hellen Jummy</div>
+                    <div class="card-author-designation">
+                      Financial Counselor
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div
-              class="col-9 d-flex flex-column justify-content-center align-items-start"
-            >
-              <div class="card-author">Hellen Jummy</div>
-              <div class="card-author-designation">Financial Counselor</div>
+            <div class="card" style="width: 20rem">
+              <div class="card-body">
+                <div class="card-content">
+                  <p class="card-text">
+                    Lacus vestibulum ultricies mi risus, duis non, volutpat
+                    nullam non. Magna congue nisi maecenas elit aliquet eu sed
+                    consectetur. Vitae quis cras vitae praesent morbi adipiscing
+                    purus consectetur mi.
+                  </p>
+                </div>
+                <div class="row mt-2">
+                  <div
+                    class="col-3 d-flex justify-content-center align-items-center user-thumb"
+                  >
+                    <img src="/assets/icons/user-thumb-1.png" alt="" />
+                  </div>
+                  <div
+                    class="col-9 d-flex flex-column justify-content-center align-items-start"
+                  >
+                    <div class="card-author">Hellen Jummy</div>
+                    <div class="card-author-designation">
+                      Financial Counselor
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card" style="width: 20rem">
+              <div class="card-body">
+                <div class="card-content">
+                  <p class="card-text">
+                    Lacus vestibulum ultricies mi risus, duis non, volutpat
+                    nullam non. Magna congue nisi maecenas elit aliquet eu sed
+                    consectetur. Vitae quis cras vitae praesent morbi adipiscing
+                    purus consectetur mi.
+                  </p>
+                </div>
+                <div class="row mt-2">
+                  <div
+                    class="col-3 d-flex justify-content-center align-items-center user-thumb"
+                  >
+                    <img src="/assets/icons/user-thumb-1.png" alt="" />
+                  </div>
+                  <div
+                    class="col-9 d-flex flex-column justify-content-center align-items-start"
+                  >
+                    <div class="card-author">Hellen Jummy</div>
+                    <div class="card-author-designation">
+                      Financial Counselor
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="card" style="width: 20rem">
-        <div class="card-body">
-          <div class="card-content">
-            <p class="card-text">
-              Lacus vestibulum ultricies mi risus, duis non, volutpat nullam
-              non. Magna congue nisi maecenas elit aliquet eu sed consectetur.
-              Vitae quis cras vitae praesent morbi adipiscing purus consectetur
-              mi.
-            </p>
-          </div>
-          <div class="row mt-2">
-            <div
-              class="col-3 d-flex justify-content-center align-items-center user-thumb"
-            >
-              <img src="/assets/icons/user-thumb-1.png" alt="" />
+        <div class="carousel-item">
+          <div
+            class="cards d-flex justofy-content-start align-items-center gap-3 py-3"
+          >
+            <div class="card" style="width: 20rem">
+              <div class="card-body">
+                <div class="card-content">
+                  <p class="card-text">
+                    ONE Lacus vestibulum ultricies mi risus, duis non, volutpat
+                    nullam non. Magna congue nisi maecenas elit aliquet eu sed
+                    consectetur. Vitae quis cras vitae praesent morbi adipiscing
+                    purus consectetur mi.
+                  </p>
+                </div>
+                <div class="row mt-2">
+                  <div
+                    class="col-3 d-flex justify-content-center align-items-center user-thumb"
+                  >
+                    <img src="/assets/icons/user-thumb-1.png" alt="" />
+                  </div>
+                  <div
+                    class="col-9 d-flex flex-column justify-content-center align-items-start"
+                  >
+                    <div class="card-author">Hellen Jummy</div>
+                    <div class="card-author-designation">
+                      Financial Counselor
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div
-              class="col-9 d-flex flex-column justify-content-center align-items-start"
-            >
-              <div class="card-author">Hellen Jummy</div>
-              <div class="card-author-designation">Financial Counselor</div>
+            <div class="card" style="width: 20rem">
+              <div class="card-body">
+                <div class="card-content">
+                  <p class="card-text">
+                    Lacus vestibulum ultricies mi risus, duis non, volutpat
+                    nullam non. Magna congue nisi maecenas elit aliquet eu sed
+                    consectetur. Vitae quis cras vitae praesent morbi adipiscing
+                    purus consectetur mi.
+                  </p>
+                </div>
+                <div class="row mt-2">
+                  <div
+                    class="col-3 d-flex justify-content-center align-items-center user-thumb"
+                  >
+                    <img src="/assets/icons/user-thumb-1.png" alt="" />
+                  </div>
+                  <div
+                    class="col-9 d-flex flex-column justify-content-center align-items-start"
+                  >
+                    <div class="card-author">Hellen Jummy</div>
+                    <div class="card-author-designation">
+                      Financial Counselor
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card" style="width: 20rem">
+              <div class="card-body">
+                <div class="card-content">
+                  <p class="card-text">
+                    Lacus vestibulum ultricies mi risus, duis non, volutpat
+                    nullam non. Magna congue nisi maecenas elit aliquet eu sed
+                    consectetur. Vitae quis cras vitae praesent morbi adipiscing
+                    purus consectetur mi.
+                  </p>
+                </div>
+                <div class="row mt-2">
+                  <div
+                    class="col-3 d-flex justify-content-center align-items-center user-thumb"
+                  >
+                    <img src="/assets/icons/user-thumb-1.png" alt="" />
+                  </div>
+                  <div
+                    class="col-9 d-flex flex-column justify-content-center align-items-start"
+                  >
+                    <div class="card-author">Hellen Jummy</div>
+                    <div class="card-author-designation">
+                      Financial Counselor
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="card" style="width: 20rem">
-        <div class="card-body">
-          <div class="card-content">
-            <p class="card-text">
-              Lacus vestibulum ultricies mi risus, duis non, volutpat nullam
-              non. Magna congue nisi maecenas elit aliquet eu sed consectetur.
-              Vitae quis cras vitae praesent morbi adipiscing purus consectetur
-              mi.
-            </p>
-          </div>
-          <div class="row mt-2">
-            <div
-              class="col-3 d-flex justify-content-center align-items-center user-thumb"
-            >
-              <img src="/assets/icons/user-thumb-1.png" alt="" />
-            </div>
-            <div
-              class="col-9 d-flex flex-column justify-content-center align-items-start"
-            >
-              <div class="card-author">Hellen Jummy</div>
-              <div class="card-author-designation">Financial Counselor</div>
-            </div>
-          </div>
-        </div>
-      </div>
       <!-- 
       <div class="card" style="width: 20rem">
         <div class="card-body">
@@ -156,17 +268,14 @@
       </div>
        -->
     </div>
-
-    <!-- Caraousel Cards -->
-
-    <!-- Caraousel Cards -->
+    <!-- </div> -->
   </div>
 </template>
 
 <style scoped>
 .arrow-btn {
   border: none;
-  /* align-items: center; */
+  background-color: white;
 }
 .arrow-btn img {
   height: 40px;
