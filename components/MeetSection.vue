c41e622c85153d507d1b6dbb92033e79a659d190
Gyan Prakash Tiwari <gyanprakashtiwari@MacBookAir-482.lan>
2025-01-13 19:22:29 +0530

    update

--- components/MeetSection.vue
+++ components/MeetSection.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="">
-    <div class="row mt-5">
-      <div class="col-lg-6 col-md-12 left">
+    <div class="row my-5">
+      <div class="col-lg-7 col-md-12 left">
         <div class="custom-bg"></div>
       </div>
       <div
-        class="col-lg-6 col-md-12 right d-flex flex-column justify-content-center align-items-start"
+        class="col-lg-5 col-md-12 right d-flex flex-column justify-content-center align-items-start"
       >
-        <!-- <div
-          class="d-flex flex-column justify-content-center align-items-start"
-        > -->
         <div class="aeroplane">
           <img src="/assets/icons/aeroplane.png" alt="" />
         </div>
@@ -30,7 +27,6 @@
             <img src="/assets/icons/right-arrow.png" alt="" />
           </a>
         </div>
-        <!-- </div> -->
       </div>
     </div>
   </div>
@@ -44,6 +40,11 @@
 .right {
   border: 2px solid black;
   border: none;
+}
+.right h1 {
+  font-size: 56px;
+  font-weight: 800;
+  color: #0f172a;
 }
 .custom-bg {
   background-image: url(/assets/icons/sec-4-left.png);
